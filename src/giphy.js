4d219b5f63b8d58a11e57771e79929349801b137
Emil Pedersen <emil.pedersen@stretch.se>
2021-03-23 13:19:09 +0100

    fixing infinite scroll

--- src/giphy.js
+++ src/giphy.js
@@ -1,11 +1,17 @@
+"use strict";
 const testApi = "https://rickandmortyapi.com/api/character";
 const apiKey = "GzN8VM3Dsp6i2X8OeYQYhcRgH9BosP6T";
 let offset = 0;
+let limit = 20;
 let allGiphys = [];
-let nextImageIndex = undefined;
-let previousImageIndex = undefined;
-const realGiphyApi = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=10&offset=${offset}`;
+let fetchedGiphys = [];
+let totalAmountOfGiphys = undefined;
+let nextModalImgId = undefined;
+let previousModalImgId = undefined;
+let showOnscrollSpinner = false;
+let realGiphyApi = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=${limit}&offset=${offset}`;
 
+let reload = document.querySelector(".reload");
 let grid = document.querySelector(".grid");
 let spinner = document.querySelector(".spinner");
 let modalBkg = document.querySelector(".modal-bkg");
@@ -13,6 +19,7 @@ let modalContent = document.querySelector(".modal-content");
 let primaryModalImg = document.querySelector(".primary-image");
 let previousModalImg = document.querySelector(".previous-image");
 let nextModalImg = document.querySelector(".next-image");
+let observableEl = document.querySelector(".observable");
 
 // mockData.map((gif) => {
 //   let card = document.createElement("div");
@@ -23,16 +30,30 @@ let nextModalImg = document.querySelector(".next-image");
 //   grid.appendChild(card);
 // });
 
-function fetchRandomGiphys(isInfiniteScroll = false) {
-  showSpinner(true, isInfiniteScroll);
+reload.addEventListener("click", reloadGiphys);
+
+const observer = new IntersectionObserver(async ([entry]) => {
+  if (entry && entry.isIntersecting) {
+    await onscroll();
+    showOnscrollSpinner = true;
+  }
+});
+
+observer.observe(observableEl);
+
+function fetchRandomGiphys() {
+  showSpinner(true, showOnscrollSpinner);
   return new Promise((res, rej) => {
-    fetch(testApi, {
-      method: "GET",
-    })
+    fetch(
+      `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=${limit}&offset=${offset}`,
+      {
+        method: "GET",
+      }
+    )
       .then((response) => response.json())
       .then((json) => {
-        console.log(json.results);
-        res(json.results);
+        totalAmountOfGiphys = json.pagination.total_count;
+        res({ data: json.data, pagination: json.pagination });
         showSpinner(false);
       })
       .catch((err) => {
@@ -41,13 +62,14 @@ function fetchRandomGiphys(isInfiniteScroll = false) {
   });
 }
 
-function showSpinner(isFetching, isInfiniteScroll) {
+function showSpinner(isFetching, showOnscrollSpinner) {
   if (!isFetching) {
-    spinner.style.display = "none";
+    spinner.classList.remove("reload");
+    spinner.classList.remove("infinite-scroll");
     return;
   }
 
-  if (isFetching && !isInfiniteScroll) {
+  if (isFetching && !showOnscrollSpinner) {
     spinner.classList.remove("infinite-scroll");
     spinner.classList.add("reload");
     return;
@@ -56,36 +78,59 @@ function showSpinner(isFetching, isInfiniteScroll) {
   spinner.classList.add("infinite-scroll");
 }
 
-fetchRandomGiphys()
-  .then((result) => {
-    allGiphys = result.map((gif, i) => {
+function reloadGiphys() {
+  allGiphys = [];
+  offset = offset > totalAmountOfGiphys ? 0 : offset + 20;
+  showOnscrollSpinner = false;
+  removeAllGiphys(grid);
+}
+
+function removeAllGiphys(parentEl) {
+  while (parentEl.firstChild) {
+    parentEl.removeChild(parentEl.firstChild);
+  }
+}
+
+async function onscroll() {
+  try {
+    let result = await fetchRandomGiphys();
+
+    fetchedGiphys = result.data.map((gif, i) => {
       let card = document.createElement("div");
       card.classList.add("card");
       let image = document.createElement("img");
-      image.src = gif.image;
+      image.src = gif.images.fixed_height.url;
       card.addEventListener("click", () => {
-        onOpneModal(i);
+        onOpneModal(gif.id);
       });
       card.appendChild(image);
       grid.appendChild(card);
-      return { id: i, image: gif.image };
+      return { id: gif.id, image: gif.images.fixed_height.url };
     });
-  })
-  .finally(() => {
-    console.log(allGiphys);
-  });
 
-function onOpneModal(imgIndex) {
-  modalBkg.classList.add("is-active");
-  getModalImages(imgIndex);
+    if (offset > result.pagination.total_count) {
+      observer.unobserve(observableEl);
+      return;
+    }
+
+    allGiphys = [...allGiphys, ...fetchedGiphys];
+    offset += 20;
+  } catch (err) {
+    console.error(err);
+  }
 }
-function getModalImages(imgIndex) {
+
+function onOpneModal(imgId) {
+  modalBkg.classList.add("is-active");
+  getModalImages(imgId);
+}
+function getModalImages(imgId) {
   allGiphys.forEach((giphy, i, arr) => {
-    if (imgIndex !== i) return;
+    if (imgId !== giphy.id) return;
     primaryModalImg.src = giphy.image;
     if (i > 0) {
-      previousImageIndex = i - 1;
-      previousModalImg.src = arr[previousImageIndex].image;
+      previousModalImgId = arr[i - 1].id;
+      previousModalImg.src = arr[i - 1].image;
       previousModalImg.addEventListener("click", previousImg, false);
     } else {
       // Sätt en default bild
@@ -93,8 +138,8 @@ function getModalImages(imgIndex) {
     }
 
     if (i < arr.length - 1) {
-      nextImageIndex = i + 1;
-      nextModalImg.src = arr[nextImageIndex].image;
+      nextModalImgId = arr[i + 1].id;
+      nextModalImg.src = arr[i + 1].image;
       nextModalImg.addEventListener("click", nextImg, false);
     } else {
       // Sätt en default bild
@@ -103,10 +148,10 @@ function getModalImages(imgIndex) {
   });
 }
 function previousImg(e) {
-  getModalImages(previousImageIndex);
+  getModalImages(previousModalImgId);
 }
 function nextImg(e) {
-  getModalImages(nextImageIndex);
+  getModalImages(nextModalImgId);
 }
 function closeModal(e) {
   modalBkg.classList.remove("is-active");
